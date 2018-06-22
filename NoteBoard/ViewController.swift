b687ef7981cbd0f4d7fa0cd45b2a9f066bb7e09a
Marcello Mirsal <dev.marcello14@gmail.com>
2018-06-22 03:51:39 +0300

    Initial Commit

---
+++ NoteBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoteBoard
+//
+//  Created by Marcello Mirsal on 22/06/2018.
+//  Copyright © 2018 Marcello Mirsal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
