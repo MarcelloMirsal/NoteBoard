d6e8bd2b8c27a414755e33a3f01bcf408fa7150e
Marcello Mirsal <dev.marcello14@gmail.com>
2018-06-22 04:05:52 +0300

    adding iOS Test Bundle

---
+++ NoteBoardTests/NotesViewControllerTests.swift
@@ -0,0 +1,27 @@
+//
+//  NoteBoardTests.swift
+//  NoteBoardTests
+//
+//  Created by Marcello Mirsal on 22/06/2018.
+//  Copyright © 2018 Marcello Mirsal. All rights reserved.
+//
+
+import XCTest
+@testable import NoteBoard
+
+class NotesViewControllerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    
+    
+    
+}
